ee1dc85b981ac7f52bc685068167f1e2b1b1b107
Mohammad <mohammad.ghorbani@hotmail.com>
2023-01-03 09:42:24 +0330

    add content to example-1

--- example-1.py
+++ example-1.py
@@ -2,3 +2,15 @@
 # We want to calculate max of their output.
 # You should calculate the output of those function in concurrent running.
 
+import time
+import random
+
+def f():
+    time.sleep(5)
+    return random.choice([1,3,5,7,9])
+
+def g():
+    time.sleep(8)
+    return random.choice([2,4,6,8,10])
+
+print(max(f(), g()))
