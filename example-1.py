8b7229fc82824aa843fcf9e97bf25f2b9924b6bf
Mohammad <mohammad.ghorbani@hotmail.com>
2023-01-03 09:44:21 +0330

    minor change

--- example-1.py
+++ example-1.py
@@ -5,12 +5,15 @@
 import time
 import random
 
+
 def f():
     time.sleep(5)
-    return random.choice([1,3,5,7,9])
+    return random.choice([1, 3, 5, 7, 9])
+
 
 def g():
     time.sleep(8)
-    return random.choice([2,4,6,8,10])
+    return random.choice([2, 4, 6, 8, 10])
+
 
 print(max(f(), g()))
